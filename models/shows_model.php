c4412cf4509be5fc08edc5d56ddf1bac6f1f6082
choper78rus666 <driver-tt@mail.ru>
2018-01-30 20:44:10 +0300

    удалил удаленные

--- models/shows_model.php
+++
@@ -1,8 +0,0 @@
-<?
-include "reader_model.php";
-$shows_json = getDataFromFile('files/item_base.txt');
-$shows_json .= ']';
-
-$item = json_decode($shows_json, true); // преаброзуем JSON с true не в объекты
-
-?>
