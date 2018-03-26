f2ef2f34633ad0be810581c59d99c43cf797449b
choper78rus666 <driver-tt@mail.ru>
2018-03-26 15:43:55 +0300

    Избавляемся от таблиц

--- private/Views/info_account.php
+++ private/Views/info_account.php
@@ -9,74 +9,73 @@
                             Информация о пользователе
                         </div>
                         <br>
-                        <table class=lk_form>
-                            <tr>
-                                <td>
+                        <div class="direction_col lk_form">
+                            <div class="row_container">
+                                <div class="flex1 fix_col">
                                     <label for="name">Имя</label>
-                                </td>
-                                <td>
+                                </div>
+                                <div class="flex1 fix_col">
                                     <input id="name" type="text" value="<? echo $user_info['name']; ?>" required>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
+                                </div>
+                            </div>
+                            <div class="row_container">
+                                <div class="flex1 fix_col">
                                     <label for="surname">Фамилия</label>
-                                </td>
-                                <td>
+                                </div>
+                                <div class="flex1 fix_col">
                                     <input id="surname" type="text" value="<? echo $user_info['surname']; ?>" required>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
+                                </div>
+                            </div>
+                            <div class="row_container">
+                                <div class="flex1 fix_col">
                                     <label for="middle_name">Отчество</label>
-                                </td>
-                                <td>
+                                </div>
+                                <div class="flex1 fix_col">
                                     <input id="middle_name" type="text" value="<? echo $user_info['middle_name']; ?>" required>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
+                                </div>
+                            </div>
+                            <div class="row_container">
+                                <div class="flex1 fix_col">
                                     <label for="birthday">Дата рождения</label>
-                                </td>
-                                <td>
+                                </div>
+                                <div class="flex1 fix_col">
                                     <input id="birthday" type="date" value="<? echo $user_info['birthday']; ?>" required>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
+                                </div>
+                            </div>
+                            <div class="row_container">
+                                <div class="flex1 fix_col">
                                     <label id="sex">Укажите ваш пол</label>
-                                </td>
-                                <td>
+                                </div>
+                                <div class="flex1">
                                     <label><input id="sexm" type="radio" name="sex" <? echo $user_info['sex'] !== male ?:'checked'; ?>>мужской</label>
                                     <label><input id="sexf" type="radio" name="sex" <? echo $user_info['sex'] !== female ?:'checked'; ?>>женский</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
+                                </div>
+                            </div>
+                            <div class="row_container">
+                                <div class="flex1 fix_col">
                                     <label for="about">Немного о себе</label>
-                                </td>
-                                <td>
+                                </div>
+                                <div class="flex1">
                                     <textarea id="about" cols="30" rows="5" maxlength="150" required><? echo $user_info['about']; ?></textarea>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
+                                </div>
+                            </div>
+                            <div class="row_container center">
+                                <div>
                                     <div><img id="image" src="../static/upload/<? echo $user_info['avatar']; ?>" height="150px"></div>
-                                    </td><td>
+                                    <br>
                                     <label>Загрузите аватар<br></label>
                                     <input id="avatar" type="file" name="avatar" accept="image/*"  value="<? echo $user_info['avatar']; ?>">
                                     <div id="response_image"></div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>
+                                </div>
+                            </div>
+                                <br>
+                            <div class="row_container center">
+                                <div>
                                     <input form="user_info" type="submit" value="Сохранить данные">
                                     <div id="response"></div>
-                                    <a style="float: right;" href="/account/login/logout">Выйти с ЛК</a>
-                                </td>
-                            </tr>
-                        </table>
+                                </div>
+                            </div>
+                        </div>
                     </fieldset>
                 </form>
             </div>
